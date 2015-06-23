708f4bcd29631ca5e90c280271c5a1d4fb1125ee
alinanmorariu <alinan.morariu@yahoo.com>
2015-06-23 12:01:34 +0200

    toggle runners details, delete runner from data table

--- public/controllers/data/runners/runnersController.js
+++ public/controllers/data/runners/runnersController.js
@@ -1,7 +1,43 @@
 var runners = angular.module ('runners', []);
 
-runners.controller('runnersController', ['$scope', '$http', function($scope, $http) {	
+runners.controller('runnersController', ['$scope', '$http', '$window', function($scope, $http, $window) {	
 		$http.get('/runners').success(function(response){
 			$scope.runners = response;
 		});	
+		
+	$scope.removeRunner = function(id){
+			var confirmDeletion = confirm('Are you sure you want to delete runner ' + id + '?');
+			if (confirmDeletion == true) {
+					$http.delete('/runners/' + id).success(function(response){
+						$window.location.reload();
+				})
+				return true;
+			}
+			else {
+				return false;
+			}	
+	};
+	
+	$scope.editRunner = function(id){
+		$http.get('/runners/' + id).success(function(response){
+			$scope.runner = response;	
+		})
+	};
+	
+	$scope.saveRunner = function(){
+		var confirmUpdate = confirm ('Are you sure you want to make changes to runner ' + $scope.runner._id + '?');
+		if (confirmUpdate == true){				
+				$http.put('/runners/' + $scope.runner._id, $scope.runner).success(function(){			
+			})
+			return true;
+		}
+		else {
+			return false;
+		}
+	};
+	
+	$scope.isCollapsed = true;
+	$scope.cancel = function(){
+		$scope.isCollapsed = true;
+	}
 }]);
