d5f7afd5faa35f15c376282138f5fe6cd39ee992
alinanmorariu <alinan.morariu@yahoo.com>
2015-08-03 15:30:33 +0200

    calculate variable donation ammount

--- public/controllers/forms/supportersFormController.js
+++ public/controllers/forms/supportersFormController.js
@@ -10,6 +10,13 @@ runnersForm.controller('supportersFormController', ['$scope', '$http', '$window'
 	
 	//submit new individual supporter
 	$scope.addSupporter = function(){
+		if ($scope.supporter.runner.indexOf("Semimaraton") > -1){
+			$scope.supporter.variableSum = Number($scope.supporter.sum) * 21;
+		}
+		else {
+			$scope.supporter.variableSum = Number($scope.supporter.sum) * 4;
+		}
+		
 		$http.post('/supporters', $scope.supporter).success(function(response){
 			alert('Your registration has been submitted! Thank you!');
 			$window.location.reload();
